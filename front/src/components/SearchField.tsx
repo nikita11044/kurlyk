8612c90f1c5694dd7ae714eb9a17f845c7fb175e
niskorostinskii <nikita11042000@gmail.com>
2021-12-10 07:07:12 +0300

    fixed search field border

--- front/src/components/SearchField.tsx
+++ front/src/components/SearchField.tsx
@@ -1,27 +1,16 @@
 import React from 'react'
 import {styled} from "@mui/styles";
-import {TextField} from '@mui/material';
+import {InputBase} from '@mui/material';
 
-export const SearchField = styled(TextField)(() => ({
-    borderRadius: 30,
-    backgroundColor: '#e6ecf0',
-    '& .Mui-focused': {
-        backgroundColor: '#fff',
-        '& fieldset': {
-            borderWidth: 1,
-            borderRadius: 30,
-            borderColor: 'rgb(29, 161, 242)'
-        },
-        '& svg path': {
-            fill: 'rgb(29, 161, 242)'
+export const SearchField = styled(InputBase)(() => ({
+    '& input': {
+        borderRadius: 30,
+        backgroundColor: '#e6ecf0',
+        padding: '14px',
+        border: '1px solid transparent',
+        '&:focus': {
+            border: '1px solid rgb(29, 161, 242)',
+            backgroundColor: '#fff',
         }
     },
-    '& fieldset': {
-        borderColor: "transparent",
-        borderRadius: 30,
-        borderWidth: 1
-    },
-    '& .MuiOutlinedInput-input': {
-        padding: '12px 14px 14px 5px'
-    }
 }))
